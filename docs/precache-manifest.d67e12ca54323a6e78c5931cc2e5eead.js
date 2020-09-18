aa7ff19df9e740aab4656d7b5f9490e7f0fdcc2a
adilet <adilet.kubatbekov@sibers.com>
2020-09-18 22:30:07 +0600

    added docs

---
+++ docs/precache-manifest.d67e12ca54323a6e78c5931cc2e5eead.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b045586029b9be39141f77efa1597d8",
+    "url": "/fankiTest/index.html"
+  },
+  {
+    "revision": "b05e62fee00d791d35d4",
+    "url": "/fankiTest/static/css/main.522c6e7a.chunk.css"
+  },
+  {
+    "revision": "5e3b0796921b3839c737",
+    "url": "/fankiTest/static/js/2.7541de80.chunk.js"
+  },
+  {
+    "revision": "b834ec6e7cc88b859dc1aafe0ac75023",
+    "url": "/fankiTest/static/js/2.7541de80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b05e62fee00d791d35d4",
+    "url": "/fankiTest/static/js/main.8c72463a.chunk.js"
+  },
+  {
+    "revision": "24abf296a57ff2d1158c",
+    "url": "/fankiTest/static/js/runtime-main.cec343f8.js"
+  },
+  {
+    "revision": "976191202670655e886b8169f822ee9c",
+    "url": "/fankiTest/static/media/books.97619120.svg"
+  },
+  {
+    "revision": "092e2d69ba181cb1722e646526294ef9",
+    "url": "/fankiTest/static/media/delphin.092e2d69.svg"
+  },
+  {
+    "revision": "e1b085659eb35d1beb415d09e8dcfbb9",
+    "url": "/fankiTest/static/media/fanki.e1b08565.svg"
+  },
+  {
+    "revision": "b6e971a04f76eef271467c374d2c141b",
+    "url": "/fankiTest/static/media/fankiIcon.b6e971a0.svg"
+  },
+  {
+    "revision": "bcb5fa13b77ceebbcfd116fad7e0ffb8",
+    "url": "/fankiTest/static/media/partypoper.bcb5fa13.svg"
+  },
+  {
+    "revision": "0c1c5a9218b0b96071f7f1b62fbf6e8e",
+    "url": "/fankiTest/static/media/smile.0c1c5a92.svg"
+  },
+  {
+    "revision": "9a61e199d79510d316af927870775365",
+    "url": "/fankiTest/static/media/updateImg.9a61e199.svg"
+  }
+]);
